eb4c6e944e8a75084a9692a58fd2b1434a2640be
Aryan Patil <aryanpatil99999999@gmail.com>
2026-09-25 18:32:37 +0530

    Add Backblaze S3 client configuration

---
+++ backend/src/main/java/com/securestorage/config/BackblazeConfig.java
@@ -0,0 +1,42 @@
+package com.securestorage.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class BackblazeConfig {
+
+    @Value("${b2.endpoint}")
+    private String endpoint;
+
+    @Value("${b2.region}")
+    private String region;
+
+    @Value("${b2.key-id}")
+    private String keyId;
+
+    @Value("${b2.application-key}")
+    private String applicationKey;
+
+    @Bean
+    public S3Client s3Client() {
+
+        AwsBasicCredentials credentials =
+                AwsBasicCredentials.create(keyId, applicationKey);
+
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(credentials)
+                )
+                .build();
+    }
+}
